1ea21605ea935bc74ff6613682b1ef7a4a0c2a60
Andrei Ignat <ignatandrei@yahoo.com>
2026-01-10 10:47:07 +0200

    Update modifyPostData.ps1

--- src/BBH/modifyPostData.ps1
+++ src/BBH/modifyPostData.ps1
@@ -6,9 +6,10 @@ Set-Content -Path $indexFilePath -Value $updatedContent
 #TODO: delete br and gz files
 Remove-Item -Path "Release/UIBlazor/wwwroot/index.html.br"
 Remove-Item -Path "Release/UIBlazor/wwwroot/index.html.gz"
+# handle 404.html
+Copy-Item -Path $indexFilePath -Destination "Release/UIBlazor/wwwroot/404.html"
 
-
-Write-Host "modify app base"
+Write-Host "modify app base back"
 $settingsFilePath = "BBH/wwwroot/appsettings.json"
 $settingsContent = Get-Content -Path $settingsFilePath -Raw
 $updatedSettingsContent = $settingsContent -replace '"AppBasePath": "/BlazorBrowserHistory/"','"AppBasePath": "/"'
