cbfad1a2c742e3c47e68d9c17d07cd0936e4ad16
Andrei Ignat <ignatandrei@yahoo.com>
2026-01-10 21:19:51 +0200

    adding sqlite persistence to opfs

---
+++ src/BBH/BBH.UI/BBHContextSqlite.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Globalization;
+
+namespace BBH.UI;
+
+public class BBHContextSqlite : DbContext
+{
+    public DbSet<BrowserUserHistoryData> BrowserUserHistoryData { get; set; }
+    public BBHContextSqlite(DbContextOptions<BBHContextSqlite> options) : base(options) { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<BrowserUserHistoryData>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Url).IsRequired().HasMaxLength(2048);
+            entity.Property(e => e.Date).HasConversion<string>();
+        });
+    }
+}
